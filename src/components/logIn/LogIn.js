a5a8a6a9b1000ab300b427300c4ca70b865cce1d
Reldalk <Reldalk@gmail.com>
2019-01-15 13:01:20 -0500

    bottom portion of login page

--- src/components/logIn/LogIn.js
+++ src/components/logIn/LogIn.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Route, withRouter} from 'react-router-dom';
-
+import './LogIn.css';
+import { required } from '../../validators';
 export class LogIn extends React.Component {
     componentDidUpdate(prevProps) {
         if (!prevProps.loggedIn && this.props.loggedIn) {
@@ -14,8 +15,45 @@ export class LogIn extends React.Component {
     }
     render() {
         return (
-            <div className="app">
+          <div className="background">
+            <div className="centerTitle">
+              <h3 className="title">Billable</h3>
             </div>
+            <div className="descriptionBox">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Nullam tortor nulla, rhoncus at laoreet quis, pretium at 
+                 elit. Nunc posuere vel metus a feugiat. Etiam dignissim erat
+                  ut neque accumsan, mattis vestibulum ex auctor. Nam accumsan 
+                  sollicitudin odio id mollis. Nunc mi diam, convallis a nisi et, 
+                  molestie ultricies urna. Etiam sodales ut est ut tincidunt.</p>
+            </div>
+            <div className="featureContainer">
+              <div className="featureBox">
+                <img className="featureImages" src={require("../../resources/Pikachu.jpg")}></img>
+                <h3>Feature</h3>
+                <div className="featureText">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Nullam tortor nulla, rhoncus at laoreet quis,</p>
+                 </div>
+              </div>
+              <div className="featureBox">
+                <img className="featureImages" src={require("../../resources/Pikachu.jpg")}></img>
+                <h3>Feature</h3>
+                <div className="featureText">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Nullam tortor nulla, rhoncus at laoreet quis,</p>
+                 </div>
+              </div>
+              <div className="featureBox">
+                <img className="featureImages" src={require("../../resources/Pikachu.jpg")}></img>
+                <h3>Feature</h3>
+                <div className="featureText">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Nullam tortor nulla, rhoncus at laoreet quis,</p>
+                 </div>
+              </div>
+            </div>
+          </div>
         );
     }
 }
