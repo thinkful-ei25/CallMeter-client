5b66748b3aac17458afe482853814c8bde756755
Reldalk <Reldalk@gmail.com>
2019-01-17 17:57:53 -0500

    missing import

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link, Route, withRouter } from 'react-router-dom';
+import { Link, Route, withRouter, Redirect } from 'react-router-dom';
 import './LandingPage.css';
 
 export class LandingPage extends React.Component {
