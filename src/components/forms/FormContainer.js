b8d7ad522b09163c639928120512d23e4c03b181
Box <40639989+theBoxed@users.noreply.github.com>
2019-01-25 22:45:49 -0500

    consolidated forms

--- src/components/forms/FormContainer.js
+++ src/components/forms/FormContainer.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
-import '../forms.css';
-import loginImg from '../loginImg.jpg';
-
+import { Redirect } from 'react-router-dom';
+import './forms.css';
+import loginImg from './loginImg.jpg';
+import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
+import { Route } from 'react-router-dom';
 
 export function Registration(props) {
-  // If we are logged in (which happens automatically when registration
-  // is successful) redirect to the user's dashboard
   if (props.loggedIn) {
     return <Redirect to="/dashboard" />;
   }
-
   const imgStyle = {
     backgroundImage: 'url(' + loginImg + ')'
   };
@@ -22,7 +20,8 @@ export function Registration(props) {
       <div className="form-container">
         <div className="login-signup-img" style={imgStyle} />
         <div className="login-signup-form-wrapper pad-50">
-          <RegisterForm />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
         </div>
       </div>
     </div>
