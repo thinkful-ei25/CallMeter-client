4de2f6efd34d3673d16f869064702f43c6fa1ccc
Sean Nealon <signalflowsean@gmail.com>
2019-01-28 22:32:29 -0500

    got log out button to show

--- src/components/containers/AppContainer.js
+++ src/components/containers/AppContainer.js
@@ -1,9 +1,9 @@
 import React from 'react'; 
 import { connect } from 'react-redux';
-import { refreshAuthToken } from '../../actions/index.actions';
-import { RequiresLogin } from '../_utils/index._utils'; 
-import { AppHeader } from '../navigation/index.navigation'; 
-
+import { refreshAuthToken, clearAuth } from '../../actions/index.actions';
+import { RequiresLogin} from '../_utils/index._utils'; 
+import { clearAuthToken } from '../../_utils/index.utils'; 
+import { AppHeader, Menu } from '../navigation/index.navigation'; 
 export class AppContainer extends React.Component{ 
 
   componentDidUpdate(prevProps) {
@@ -30,26 +30,31 @@ export class AppContainer extends React.Component{
     clearInterval(this.refreshInterval);
   }
 
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
+
   render(){ 
     return (
       <div>
         <AppHeader name={this.props.organizationName} />
-        {/* <div className="app-container">
-          <Route exact path="/clients" component={Clients} />
+        <div className="app-container">
+          {/* <Route exact path="/clients" component={Clients} /> */}
           <section id="contacts">
             <div className="section-container">
-              <Route path="/dashboard" component={GlobalDialer} />
+              {/* <Route path="/dashboard" component={GlobalDialer} />
               <Route exact path="/dashboard" component={Stats} />
               <Route exact path="/dashboard/setup" component={FormContainer} />
               <Route exact path="/dashboard/stats" component={Stats} />
               <Route exact path="/dashboard/call" component={MakeCall} />
               <Route exact path="/dashboard/invoices" component={Invoices} />
-              <Route exact path="/clients/:clientId" component={ContactPage} />
+              <Route exact path="/clients/:clientId" component={ContactPage} /> */}
               <button onClick={() => this.logOut()}>LOG OUT</button>
-              <NavBar />
+              {/* <Menu /> */}
             </div>
           </section>
-        </div> */}
+        </div>
       </div>
     ); 
   }
