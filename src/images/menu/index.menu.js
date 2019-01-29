6ee68ec40b4462825bc6e125e1177c16eca503c9
Sean Nealon <signalflowsean@gmail.com>
2019-01-29 14:16:14 -0500

    added gear icon to menu images

--- src/images/menu/index.menu.js
+++ src/images/menu/index.menu.js
@@ -2,9 +2,11 @@ import calls from './calls.png';
 import invoices from './invoices.png';
 import close from './X.png'; 
 import contacts from './meeple.png'; 
+import gear from './gear.png'; 
 export {
   calls, 
   invoices, 
   close, 
-  contacts
+  contacts, 
+  gear
 }
