77d8eb0277c9a049ce8be83ba927f3af4c6c9940
Box <40639989+theBoxed@users.noreply.github.com>
2019-01-30 17:12:35 -0500

    contact page styles

---
+++ src/images/backgrounds/index.backgrounds.js
@@ -0,0 +1,7 @@
+import blackShapes from './blackShapes.jpg';
+import darkBlue_triangle from './darkBlue_triangle.jpg';
+import orange_triangle from './orange_triangle.jpg';
+import purple_shapes from './purple_shapes.jpg';
+import purple_triangle from './purple_triangle.jpg';
+
+export { blackShapes, darkBlue_triangle, orange_triangle, purple_shapes, purple_triangle };
