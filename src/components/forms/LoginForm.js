15d4deb46e708a0daf4af587f724dd6d3aed5e4f
Box <40639989+theBoxed@users.noreply.github.com>
2019-01-30 23:22:00 -0500

    forms and styles

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Field, reduxForm, focus } from 'redux-form';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Input } from '../_utils/index._utils';
 import { login } from '../../actions/index.actions';
 import { required, nonEmpty } from '../../_utils/index.utils';
