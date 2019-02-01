4e31ce6291cc21c2beaaf904e0831138bd13dcb0
Reldalk <Reldalk@gmail.com>
2019-02-01 12:21:00 -0500

    fixed settings

--- src/components/forms/UserSettings.js
+++ src/components/forms/UserSettings.js
@@ -7,7 +7,8 @@ import {
   matches,
   length,
   isTrimmed,
-  emailCheck
+  emailCheck,
+  normalizePhone
 } from '../../_utils/index.utils';
 const passwordLength = length({ min: 10, max: 72 });
 const matchesPassword = matches('password');
@@ -29,17 +30,24 @@ export class RegistrationForm extends React.Component {
           component={Input}
 					label="Change Organization Phone Number"
           type="text"
+          normalize={normalizePhone}
           name="organizationPhoneNumber"
-          validate={[required, nonEmpty, isTrimmed]}
           placeholder="Your New Phone"
         />
-         <Field
+        <Field
           component={Input}
-					label="Change Hourly Rate"
+          label="Your Hourly Rate"
+          type="number"
+          name="hourlyRate"
+          id="hourlyRate"
+          placeholder="100"
+        />
+        <Field
+          component={Input}
+          label="Email Address"
           type="text"
           name="email"
-          validate={[required, nonEmpty, isTrimmed, emailCheck]}
-          placeholder="Your New Email Address"
+          placeholder="Where can we reach you?"
         />
         <Field
 					component={Input}
@@ -47,7 +55,6 @@ export class RegistrationForm extends React.Component {
           type="password"
 					name="password"
 					id="password"
-          validate={[required, passwordLength, isTrimmed]}
           placeholder="************"
         />
         <Field
@@ -56,7 +63,6 @@ export class RegistrationForm extends React.Component {
           type="password"
           name="passwordConfirm"
           placeholder="************"
-          validate={[required, nonEmpty, matchesPassword]}
         />
 				<div className="form-button-container">
 					<div className="form-button-wrapper">
